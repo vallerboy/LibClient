fc43ba065d37a4cbb962979c0d7158596066ac91
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-06-05 20:39:08 +0200

    ddd

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -7,49 +7,58 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.awt.print.Book;
+import java.util.Scanner;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-
-
-    }
-
+public class Main {
 
     public static void main(String[] args) {
-        //System.out.println(Utils.makeRequest("http://localhost:8080/book"));
+        Scanner scanner = new Scanner(System.in);
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
 
-        BookEntity bookEntity = restTemplate.getForObject("http://localhost:8080/book/2", BookEntity.class);
-        System.out.println(bookEntity.getTitle());
+        System.out.print("Podaj klucz: ");
+        String key = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Podaj ID: ");
+        int id = Integer.parseInt(scanner.nextLine());
+
+
+
+        BookEntity bookEntity = restTemplate.getForObject("http://localhost:8080/book/" + id, BookEntity.class);
+
 
         ////////////////////////////////////////
-        BookEntity bookEntity1 = new BookEntity();
-        bookEntity1.setAuthor("Ksiazka Oskara");
-        bookEntity1.setPages(5324);
-        bookEntity1.setTitle("Z aplikacji");
+        BookEntity newBookEntity = new BookEntity();
 
+        System.out.print("Podaj autora: ");
+        String author = scanner.nextLine();
+        newBookEntity.setAuthor(author.equals("") ? bookEntity.getAuthor() : author);
+
+        System.out.print("Podaj ilość stron: ");
+        int pages = Integer.parseInt(scanner.nextLine());
+        newBookEntity.setPages(pages);
+
+        System.out.print("Podaj tytuł: ");
+        String title = scanner.nextLine();
+        newBookEntity.setTitle(title.equals("") ? bookEntity.getTitle() : title);
+
+        newBookEntity.setId(id);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.add("key", "22142121o2oasfADa25noisafrj00uAIODJOSf241");
+        httpHeaders.add("key", key);
 
-        HttpEntity<BookEntity> entity = new HttpEntity<BookEntity>(bookEntity1, httpHeaders);
-        restTemplate.postForObject("http://localhost:8080/book", entity, String.class);
+        HttpEntity<BookEntity> entity = new HttpEntity<>(newBookEntity, httpHeaders);
+        restTemplate.exchange("http://localhost:8080/book", HttpMethod.PUT, entity, String.class);
         ////////////////////////////////////////
 
-       // launch(args);
     }
 }
